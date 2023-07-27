e8f12324ee915568739d914269893c585299ea30
AtharvaSPatil <121273696+AtharvaSPatil@users.noreply.github.com>
2023-07-27 22:43:45 +0530

    Daily Problem

---
+++ A_Desorting.cpp
@@ -0,0 +1,81 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define int long long
+#define endl '\n' 
+#define runtime() cerr << ((double)clock() / CLOCKS_PER_SEC) << endl;
+
+int power(int a,int b)
+{
+    if(b==0)
+    {
+        return 0;
+    }
+    else
+    {
+        int value=power(a,b/2);
+        if(b%2==0)
+            return value*value;
+        else
+            return value*value*a;
+    }
+}
+
+int gcd(int a,int b)
+{
+    if(a==0)
+        return b;
+    else
+        return gcd(b%a,a);
+}
+
+int lcm(int a,int b)
+{
+        return (a*b)/(gcd(a,b));
+}
+
+vector<int> primefactors(int n)
+{
+    vector<int> factors;
+    for(int i=2;i*i<=n;i++)
+    {
+        while(n%i==0)
+        {
+            factors.push_back(i);
+            n/=i;
+        }
+    }
+    return factors;
+}
+
+signed main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n; cin >> n;
+        vector<int> v(n);
+        int difference = 1e9;
+        bool flag = true;
+        for (int i = 0; i < n; i++) {
+            cin >> v[i];
+            if (i > 0) {
+                difference = min(v[i] - v[i - 1], difference);
+                flag &= v[i] >= v[i - 1];
+            }
+        }
+        
+        if (!flag) {
+            cout << 0 << endl;
+            continue;
+        }
+    
+        cout << difference/2 + 1 << endl;
+    }
+
+    return 0;
+}

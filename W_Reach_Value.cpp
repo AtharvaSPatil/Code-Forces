192096a7a76c5b1163fd86e58c9c2fcfa3a3e765
AtharvaSPatil <121273696+AtharvaSPatil@users.noreply.github.com>
2023-08-21 00:53:28 +0530

    Daily Problem

---
+++ W_Reach_Value.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define int long long
+#define endl '\n' 
+#define runtime() cerr << ((double)clock() / CLOCKS_PER_SEC) << endl;
+
+int power(int a,int b)
+{
+    if(b==0)
+    {
+        return 0;
+    }
+    else
+    {
+        int value=power(a,b/2);
+        if(b%2==0)
+            return value*value;
+        else
+            return value*value*a;
+    }
+}
+
+int gcd(int a,int b)
+{
+    if(a==0)
+        return b;
+    else
+        return gcd(b%a,a);
+}
+
+int lcm(int a,int b)
+{
+        return (a*b)/(gcd(a,b));
+}
+
+vector<int> primefactors(int n)
+{
+    vector<int> factors;
+    for(int i=2;i*i<=n;i++)
+    {
+        while(n%i==0)
+        {
+            factors.push_back(i);
+            n/=i;
+        }
+    }
+    return factors;
+}
+bool solve(int n,int value)
+{
+    if(value>n)
+        return false;
+    if(value==n)
+        return true;
+    return solve(n,value*20) || solve(n,value*10);
+}
+signed main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        if(solve(n,1))
+        cout<<"YES"<<endl;
+        else
+        cout<<"NO"<<endl;
+    }
+
+    return 0;
+}

36a6d249bb52a462d79aa0b0563525ed477b1eaa
AtharvaSPatil <121273696+AtharvaSPatil@users.noreply.github.com>
2023-08-20 23:24:20 +0530

    Daily Problem

---
+++ B_Effective_Approach.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define int long long
+#define endl '\n' 
+#define runtime() cerr << ((double)clock() / CLOCKS_PER_SEC) << endl;
+
+int power(int a,int b)
+{
+    if(b==0)
+    {
+        return 0;
+    }
+    else
+    {
+        int value=power(a,b/2);
+        if(b%2==0)
+            return value*value;
+        else
+            return value*value*a;
+    }
+}
+
+int gcd(int a,int b)
+{
+    if(a==0)
+        return b;
+    else
+        return gcd(b%a,a);
+}
+
+int lcm(int a,int b)
+{
+        return (a*b)/(gcd(a,b));
+}
+
+vector<int> primefactors(int n)
+{
+    vector<int> factors;
+    for(int i=2;i*i<=n;i++)
+    {
+        while(n%i==0)
+        {
+            factors.push_back(i);
+            n/=i;
+        }
+    }
+    return factors;
+}
+
+signed main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    int vasya=0,petya=0;
+    cin>>n;
+    int a,index[100013];
+    for(int i=1;i<=n;i++)
+    {
+        cin>>a;
+        index[a]=i;
+    }
+    int m,b;
+    cin>>m;
+    while(m--)
+    {
+        cin>>b;
+        vasya+=index[b];
+        petya+=(n+1-index[b]); 
+    }
+    cout<<vasya<<" "<<petya<<endl;
+    return 0;
+}

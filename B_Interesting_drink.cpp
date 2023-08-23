eaf5c4b869f86c2aa8a8ac4d47eb0e8aeafb8202
AtharvaSPatil <121273696+AtharvaSPatil@users.noreply.github.com>
2023-08-23 22:37:16 +0530

    Daily Problem

---
+++ B_Interesting_drink.cpp
@@ -0,0 +1,90 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define int long long
+#define endl '\n' 
+#define runtime() cerr << ((double)clock() / CLOCKS_PER_SEC) << endl;
+
+int power(int a,int b)
+{
+    if(b==0)
+    {
+        return 0;
+    }
+    else
+    {
+        int value=power(a,b/2);
+        if(b%2==0)
+            return value*value;
+        else
+            return value*value*a;
+    }
+}
+
+int gcd(int a,int b)
+{
+    if(a==0)
+        return b;
+    else
+        return gcd(b%a,a);
+}
+
+int lcm(int a,int b)
+{
+        return (a*b)/(gcd(a,b));
+}
+
+vector<int> primefactors(int n)
+{
+    vector<int> factors;
+    for(int i=2;i*i<=n;i++)
+    {
+        while(n%i==0)
+        {
+            factors.push_back(i);
+            n/=i;
+        }
+    }
+    return factors;
+}
+
+signed main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+	cin>>n;
+
+	vector<int> v;
+	for(int i = 0; i< n ; i++){
+		int temp;
+		cin>>temp;
+		v.push_back(temp);
+	}
+	sort(v.begin(), v.end());
+
+	int q;
+	cin>>q;
+	while(q--){
+		int p;
+		cin>>p;
+
+		int l = -1;
+		int r = n;
+
+		while(r > l + 1){
+			int m = (l + r)/2;
+			if(v[m]<= p){
+				l = m;
+			}else{
+				r = m;
+			}
+		}
+
+		cout<<l + 1<<"\n";
+	}
+
+    return 0;
+}
+

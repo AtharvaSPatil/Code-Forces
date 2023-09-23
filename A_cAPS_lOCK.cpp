8a52e39fc6a24bb033ea464924ef2dc5873ed1f0
AtharvaSPatil <121273696+AtharvaSPatil@users.noreply.github.com>
2023-09-23 21:48:42 +0530

    Daily Problem

---
+++ A_cAPS_lOCK.cpp
@@ -0,0 +1,82 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define int long long
+#define endl '\n' 
+#define runtime() cerr << ((double)clock() / CLOCKS_PER_SEC) << endl;
+
+int power(int a,int b)
+{
+    if(b==0)
+    {
+        return 0;
+    }
+    else
+    {
+        int value=power(a,b/2);
+        if(b%2==0)
+            return value*value;
+        else
+            return value*value*a;
+    }
+}
+
+int gcd(int a,int b)
+{
+    if(a==0)
+        return b;
+    else
+        return gcd(b%a,a);
+}
+
+int lcm(int a,int b)
+{
+        return (a*b)/(gcd(a,b));
+}
+
+vector<int> primefactors(int n)
+{
+    vector<int> factors;
+    for(int i=2;i*i<=n;i++)
+    {
+        while(n%i==0)
+        {
+            factors.push_back(i);
+            n/=i;
+        }
+    }
+    return factors;
+}
+
+signed main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    string s, r = "";
+    cin>>s;
+    bool check = true;
+    for(int i = 1; i < s.length(); i++)
+    {
+        if(s[i] >= 97)
+        {
+        check = false;
+        break;
+        }
+    }
+    if(s[0] <= 90 && check)
+    {
+        transform(s.begin(), s.end(), s.begin(), ::tolower);
+    }
+    else if(s[0] >= 97 && check)
+    {
+        s[0] = toupper(s[0]);
+        for(int i = 1; i < s.length(); i++)
+        {
+        s[i] = tolower(s[i]);
+        }
+    }
+    cout<<s<<endl;
+
+    return 0;
+}

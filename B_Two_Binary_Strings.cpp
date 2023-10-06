e8e602882d9876f22733fdbb88518ab7a62a0ac9
AtharvaSPatil <121273696+AtharvaSPatil@users.noreply.github.com>
2023-10-06 22:00:46 +0530

    Daily Problem

---
+++ B_Two_Binary_Strings.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define int long long
+#define endl '\n' 
+#define runtime() cerr << ((double)clock() / CLOCKS_PER_SEC) << endl;
+
+int power(int a,int b)
+{
+    if(b==0)
+    {
+        return 0;
+    }
+    else
+    {
+        int value=power(a,b/2);
+        if(b%2==0)
+            return value*value;
+        else
+            return value*value*a;
+    }
+}
+
+int gcd(int a,int b)
+{
+    if(a==0)
+        return b;
+    else
+        return gcd(b%a,a);
+}
+
+int lcm(int a,int b)
+{
+        return (a*b)/(gcd(a,b));
+}
+
+vector<int> primefactors(int n)
+{
+    vector<int> factors;
+    for(int i=2;i*i<=n;i++)
+    {
+        while(n%i==0)
+        {
+            factors.push_back(i);
+            n/=i;
+        }
+    }
+    return factors;
+}
+
+signed main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string a, b;
+        cin >> a >> b;
+        bool flag = false;
+        for (int i = 0; i + 1 < a.size(); ++i) {
+            if (a[i] == b[i] && a[i] == '0' && a[i + 1] == b[i + 1] && a[i + 1] == '1') {
+                flag = true;
+            }
+        }
+        
+        if (flag) 
+            puts("YES");
+        else
+            puts("NO");
+    }
+
+    return 0;
+}

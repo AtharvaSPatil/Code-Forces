8ab8461f7f927779f1807cadfc2675adfde4243f
AtharvaSPatil <121273696+AtharvaSPatil@users.noreply.github.com>
2023-10-13 20:53:35 +0530

    Daily Problem

---
+++ A_Don_t_Try_to_Count.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define runtime() cerr << ((double)clock() / CLOCKS_PER_SEC) << endl;
+
+int power(int a, int b)
+{
+    if (b == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        int value = power(a, b / 2);
+        if (b % 2 == 0)
+            return value * value;
+        else
+            return value * value * a;
+    }
+}
+
+int gcd(int a, int b)
+{
+    if (a == 0)
+        return b;
+    else
+        return gcd(b % a, a);
+}
+
+int lcm(int a, int b)
+{
+    return (a * b) / (gcd(a, b));
+}
+
+vector<int> primefactors(int n)
+{
+    vector<int> factors;
+    for (int i = 2; i * i <= n; i++)
+    {
+        while (n % i == 0)
+        {
+            factors.push_back(i);
+            n /= i;
+        }
+    }
+    return factors;
+}
+
+signed main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m;
+        cin >> n >> m;
+        string s, t;
+        cin >> s >> t;
+        int ans = -1;
+        for (int i = 0; i < 2 || s.size() < t.size() * 4; i++)
+        {
+            if (s.find(t) != string::npos)
+            {
+                ans = i;
+                break;
+            }
+            s += s;
+        }
+        cout << ans << '\n';
+    }
+
+    return 0;
+}

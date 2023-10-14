cb68710f14eb5bc7a032629e26796ff5cc2e7f5e
AtharvaSPatil <121273696+AtharvaSPatil@users.noreply.github.com>
2023-10-14 23:18:30 +0530

    Daily Problem

---
+++ B_The_Corridor_or_There_and_Back_Again.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int M=998244353;
+void printv(vector<ll>&a){
+    for(int i=0;i<a.size();i++){
+        cout<<a[i]<<" ";
+    }
+}
+ll binpow(ll a, ll b) {
+    if (b == 0)
+        return 1;
+    long long res = binpow(a, b / 2);
+    if (b % 2){
+        long long temp=((res%M) * (res%M))%M;
+        return ((temp%M)*a)%M;
+    }
+    else
+        return ((res%M) * (res%M))%M;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    ll t=1;
+    cin>>t;
+    while(t--){
+        ll n,m,k;
+        cin>>n;
+        vector<pair<ll,ll>>traps;
+        for(int i=0;i<n;i++){
+            cin>>m>>k;
+            traps.push_back({m,k-1});
+        }
+        sort(traps.begin(),traps.end());
+        ll maxi=traps[0].first+traps[0].second/2;
+        for(int i=1;i<traps.size();i++){
+            maxi=min(maxi,traps[i].first+traps[i].second/2);
+        }
+        cout<<maxi<<endl;
+    }
+}

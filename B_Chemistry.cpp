83186d5dd8032274e160c9006c927516773b4c35
AtharvaSPatil <121273696+AtharvaSPatil@users.noreply.github.com>
2024-01-06 23:18:30 +0530

    Daily Problems

---
+++ B_Chemistry.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define int long long
+#define endl '\n' 
+#define runtime() cerr << ((double)clock() / CLOCKS_PER_SEC) << endl;
+
+int power(int a,int b)
+{
+    if(b==0)
+    {
+        return 0;
+    }
+    else
+    {
+        int value=power(a,b/2);
+        if(b%2==0)
+            return value*value;
+        else
+            return value*value*a;
+    }
+}
+
+int gcd(int a,int b)
+{
+    if(a==0)
+        return b;
+    else
+        return gcd(b%a,a);
+}
+
+int lcm(int a,int b)
+{
+        return (a*b)/(gcd(a,b));
+}
+
+vector<int> primefactors(int n)
+{
+    vector<int> factors;
+    for(int i=2;i*i<=n;i++)
+    {
+        while(n%i==0)
+        {
+            factors.push_back(i);
+            n/=i;
+        }
+    }
+    return factors;
+}
+
+signed main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,k;
+        cin>>n>>k;
+        string s;
+        cin>>s;
+        unordered_map<char,int> umap;
+        for(int i=0;i<n;i++)
+        {
+            umap[s[i]]++;
+        }
+        int count=0;
+        for(int i=0;i<umap.size(); i++)
+        {
+            if(umap[i]%2!=0) count++;
+        }
+        if(count>k+1) cout<<"NO"<<endl;
+        else cout<<"YES"<<endl;
+    }
+
+    return 0;
+}

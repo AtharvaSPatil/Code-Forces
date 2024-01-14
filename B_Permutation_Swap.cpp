adf2f34f319b806d99eca17df3a38527414d2313
AtharvaSPatil <121273696+AtharvaSPatil@users.noreply.github.com>
2024-01-14 21:48:50 +0530

    Daily Problem

--- B_Permutation_Swap.cpp
+++ B_Permutation_Swap.cpp
@@ -4,7 +4,50 @@ using namespace std;
 
 #define int long long
 #define endl '\n' 
+#define runtime() cerr << ((double)clock() / CLOCKS_PER_SEC) << endl;
 
+int power(int a,int b)
+{
+    if(b==0)
+    {
+        return 0;
+    }
+    else
+    {
+        int value=power(a,b/2);
+        if(b%2==0)
+            return value*value;
+        else
+            return value*value*a;
+    }
+}
+
+int gcd(int a,int b)
+{
+    if(a==0)
+        return b;
+    else
+        return gcd(b%a,a);
+}
+
+int lcm(int a,int b)
+{
+        return (a*b)/(gcd(a,b));
+}
+
+vector<int> primefactors(int n)
+{
+    vector<int> factors;
+    for(int i=2;i*i<=n;i++)
+    {
+        while(n%i==0)
+        {
+            factors.push_back(i);
+            n/=i;
+        }
+    }
+    return factors;
+}
 
 signed main()
 {
@@ -21,7 +64,6 @@ signed main()
     		res = __gcd(res, abs(x - i));
     	}
     	cout << res << "\n";
-    
     }
 
     return 0;

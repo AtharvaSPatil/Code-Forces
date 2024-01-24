08e15c2779f8a3f323bf863be1049fd03af2a07d
AtharvaSPatil <121273696+AtharvaSPatil@users.noreply.github.com>
2024-01-24 22:58:35 +0530

    Daily Problem

--- A_Satisfying_Constraints.cpp
+++ A_Satisfying_Constraints.cpp
@@ -1,49 +1,48 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-
 #define int long long
-#define endl '\n' 
+#define endl '\n'
 #define runtime() cerr << ((double)clock() / CLOCKS_PER_SEC) << endl;
 
-int power(int a,int b)
+int power(int a, int b)
 {
-    if(b==0)
+    if (b == 0)
     {
         return 0;
     }
     else
     {
-        int value=power(a,b/2);
-        if(b%2==0)
-            return value*value;
+        int value = power(a, b / 2);
+        if (b % 2 == 0)
+            return value * value;
         else
-            return value*value*a;
+            return value * value * a;
     }
 }
 
-int gcd(int a,int b)
+int gcd(int a, int b)
 {
-    if(a==0)
+    if (a == 0)
         return b;
     else
-        return gcd(b%a,a);
+        return gcd(b % a, a);
 }
 
-int lcm(int a,int b)
+int lcm(int a, int b)
 {
-        return (a*b)/(gcd(a,b));
+    return (a * b) / (gcd(a, b));
 }
 
 vector<int> primefactors(int n)
 {
     vector<int> factors;
-    for(int i=2;i*i<=n;i++)
+    for (int i = 2; i * i <= n; i++)
     {
-        while(n%i==0)
+        while (n % i == 0)
         {
             factors.push_back(i);
-            n/=i;
+            n /= i;
         }
     }
     return factors;
@@ -54,10 +53,35 @@ signed main()
     ios::sync_with_stdio(0);
     cin.tie(0);
     int t;
-    cin>>t;
-    while(t--)
+    cin >> t;
+    while (t--)
     {
-        
+        int n;
+        cin >> n;
+
+        int l = 1;
+        int r = 1e9;
+        int s = 0;
+
+        vector<int> neq;
+
+        for (int i = 0; i < n; i++)
+        {
+            int a, x;
+            cin >> a >> x;
+
+            if (a == 1)
+                l = max(l, x);
+            if (a == 2)
+                r = min(r, x);
+            if (a == 3)
+                neq.push_back(x);
+        }
+        for (int x : neq)
+            if (x >= l and x <= r)
+                s++;
+
+        cout<<max((int)(r-l+1-s),(int)0)<<endl;
     }
 
     return 0;

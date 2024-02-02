31c7dc0157e7e181d2814903085436be3a0fe6d3
AtharvaSPatil <121273696+AtharvaSPatil@users.noreply.github.com>
2024-02-02 22:09:05 +0530

    Daily Problem

---
+++ C_Can_I_Square.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define runtime() cerr << ((double)clock() / CLOCKS_PER_SEC) << endl;
+
+int power(int a, int b)
+{
+    if (b == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        int value = power(a, b / 2);
+        if (b % 2 == 0)
+            return value * value;
+        else
+            return value * value * a;
+    }
+}
+
+int gcd(int a, int b)
+{
+    if (a == 0)
+        return b;
+    else
+        return gcd(b % a, a);
+}
+
+int lcm(int a, int b)
+{
+    return (a * b) / (gcd(a, b));
+}
+
+vector<int> primefactors(int n)
+{
+    vector<int> factors;
+    for (int i = 2; i * i <= n; i++)
+    {
+        while (n % i == 0)
+        {
+            factors.push_back(i);
+            n /= i;
+        }
+    }
+    return factors;
+}
+
+bool is_square(int x) {
+    int l = 1, r = 1e9;
+    while(l <= r) {
+        int mid = l + (r - l) / 2;
+        if(mid * mid == x) return true;
+        if(mid * mid > x) r = mid - 1;
+        else l = mid + 1;
+    }      
+    return false;  
+}
+
+signed main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int s = 0;
+        for (int i = 0, x; i < n; ++i)
+        {
+            cin >> x;
+            s += x;
+        }
+        if (is_square(s))
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+
+    return 0;
+}
